601ccb688100aaa41ab1eb9dfc03341fb8313b2b
Camila Ranniele <rannielecamsil@gmail.com>
2022-11-21 10:14:03 -0300

    Backend

---
+++ backend/app/controllers/estacao_controller.py
@@ -0,0 +1,81 @@
+import argparse
+
+from flask_restful import reqparse, Api, Resource, fields
+
+from app.models.estacao import EstacaoDataBase
+from app.models.estacao_schema import EstacaoDataBaseSchema
+from config import db
+
+
+parser = reqparse.RequestParser()
+parser.add_argument('id_estacao', type=int,
+                    help='identificador da estacao')
+parser.add_argument('nome_estacao', type=str, help='nome do estacao')
+parser.add_argument('codigo_regiao', type=str, help='codigo regiao')
+parser.add_argument('uf', type=str, help='UF')
+parser.add_argument('codigo_wmo', type=str, help='codigo WMO da estacao meteorologica - A001')
+parser.add_argument('latitude', type=float, help='latitude')
+parser.add_argument('longitude', type=float, help='longitude')
+parser.add_argument('altitude', type=float, help='altitude')
+parser.add_argument('data_fundacao', type=str, help='data')
+
+
+class Estacao(Resource):
+    def get(self, codigo_wmo):
+        estacao = EstacaoDataBase.query.filter_by(
+            codigo_wmo=codigo_wmo).first()
+        estacao_schema = EstacaoDataBaseSchema()
+        resp = estacao_schema.dump(estacao)
+        return {"estacao": resp}, 200
+
+    def delete(self, codigo_wmo):
+        estacao = EstacaoDataBase.query.filter_by(
+            codigo_wmo=codigo_wmo).first()
+        db.session.delete(estacao)
+        db.session.commit()
+        return 'Estação deletada', 204
+
+    def put(self, codigo_wmo):
+        estacao_json = parser.parse_args()
+        estacao = EstacaoDataBase.query.filter_by(
+            codigo_wmo=codigo_wmo).first()
+
+        estacao.nome_estacao = estacao_json.nome_estacao
+        estacao.codigo_regiao = estacao_json.codigo_regiao
+        estacao.uf = estacao_json.uf
+        estacao.codigo_wmo = estacao_json.codigo_wmo
+        estacao.latitude = estacao_json.latitude
+        estacao.longitude = estacao_json.longitude
+        estacao.altitude = estacao_json.altitude
+        estacao.data_fundacao = estacao_json.data_fundacao
+
+        db.session.add(estacao)
+        db.session.commit()
+
+        estacao_schema = EstacaoDataBaseSchema(
+            only=['id_estacao', 'nome_estacao', 'codigo_regiao', 'uf', 'codigo_wmo', 'latitude', 'longitude',
+                  'altitude', 'data_fundacao'])
+        resp = estacao_schema.dump(estacao)
+
+        return {"estacao": resp}, 200
+
+
+class ListaEstacao(Resource):
+    def get(self):
+        estacoes = EstacaoDataBase.query.all()
+        estacao_schema = EstacaoDataBaseSchema(many=True)
+        resp = estacao_schema.dump(estacoes)
+        return {"estacoes": resp}, 200
+
+    def post(self):
+        estacao_json = parser.parse_args()
+        estacao_schema = EstacaoDataBaseSchema()
+        estacao = estacao_schema.load(estacao_json)
+        estacaoDataBase = EstacaoDataBase(
+            estacao["nome_estacao"], estacao["codigo_regiao"],
+            estacao["uf"], estacao["codigo_wmo"],
+            estacao["latitude"], estacao["longitude"], estacao["altitude"],
+            estacao["data_fundacao"]
+        )
+        resp = estacao_schema.dump(estacaoDataBase.create())
+        return {"estacao": resp}, 201
---
+++ backend/main.py
@@ -0,0 +1,15 @@
+from flask_restful import Api
+
+from config import app, db
+from app.controllers.estacao_controller import Estacao, ListaEstacao
+
+
+api = Api(app)
+
+api.add_resource(ListaEstacao, '/estacao')
+api.add_resource(Estacao, '/estacao/<codigo_regiao>')
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
