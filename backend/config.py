8aae623f640a0af00a73adb341c809ea85018005
Camila Ranniele <rannielecamsil@gmail.com>
2022-11-23 11:56:54 -0300

    [backend] habilita CORS

--- backend/config.py
+++ backend/config.py
@@ -1,9 +1,11 @@
 from flask import Flask
+from flask_cors import CORS
 
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 
 app = Flask(__name__)
+CORS(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://empresa_user:123456' \
                                         '@localhost/metereologia'
 
