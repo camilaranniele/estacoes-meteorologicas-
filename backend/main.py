962832b52ad7463542c17db8c4c832799ae6896f
Camila Ranniele <rannielecamsil@gmail.com>
2022-11-23 11:57:53 -0300

    [backend] Troca URL do endpoint para recever codigo_wmo

--- backend/main.py
+++ backend/main.py
@@ -7,7 +7,7 @@ from app.controllers.estacao_controller import Estacao, ListaEstacao
 api = Api(app)
 
 api.add_resource(ListaEstacao, '/estacao')
-api.add_resource(Estacao, '/estacao/<codigo_regiao>')
+api.add_resource(Estacao, '/estacao/<codigo_wmo>')
 
 if __name__ == '__main__':
     with app.app_context():
